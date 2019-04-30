086a120deb9943f424355cbb401e52a17708ff9d
Byeonghun Hwang <byeonghwang.com>
2019-04-30 17:31:06 +0900

    2nd Study fin

--- HR/HRViewController.swift
+++ HR/HRViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 import Foundation
 
-class HRViewController : UIViewController, UITableViewDataSource {
+class HRViewController : UIViewController, UITableViewDataSource, UICollectionViewDataSource {
+    @IBOutlet weak var viewCollection:UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "HRCollectionViewCell", bundle: nil)
+        self.viewCollection.register(nib, forCellWithReuseIdentifier: "Cell")
+    }
     
     let modelHR = HRModel()
+    
+    // MARK: - 여기서부터 테이블 뷰 데이터 소스 파트
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -39,5 +48,29 @@ class HRViewController : UIViewController, UITableViewDataSource {
         
         return cell
     }
+    
+    // MARK: - 여기서부터 컬렉션 뷰 데이터 소스 파트
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.modelHR.arrayList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let dicInfo = self.modelHR.arrayList[indexPath.row] as! [String:String]
+        
+        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        //cell.backgroundColor = .red
+        (cell.viewWithTag(1) as? UILabel)?.text = dicInfo["name"]
+        (cell.viewWithTag(2) as? UILabel)?.text = dicInfo["desc"]
+        
+        return cell
+    }
+    
+    
     
 }
